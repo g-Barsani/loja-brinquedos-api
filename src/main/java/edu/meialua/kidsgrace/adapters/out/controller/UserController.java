4da5102945e18992318da53d31a4f30dc45a918a
Alisson Mayer Medeji <alissonmmedeji@gmail.com>
2025-04-03 20:27:36 -0300

    Add RegisterDto, Change SecurityConfig, UserController

--- src/main/java/edu/meialua/kidsgrace/adapters/out/controller/UserController.java
+++ src/main/java/edu/meialua/kidsgrace/adapters/out/controller/UserController.java
@@ -2,13 +2,22 @@ package edu.meialua.kidsgrace.adapters.out.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.meialua.kidsgrace.adapters.in.Role;
 import edu.meialua.kidsgrace.adapters.in.User;
+import edu.meialua.kidsgrace.adapters.in.repositories.RoleRepository;
 import edu.meialua.kidsgrace.adapters.in.repositories.UserRepository;
+import edu.meialua.kidsgrace.model.RegisterDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,11 +25,21 @@ import java.util.Optional;
 @RequestMapping("/users")
 public class UserController {
 
-    @Autowired
+    private AuthenticationManager authenticationManager;
     private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     ObjectMapper objectMapper;
+
+    @Autowired
+    public UserController(AuthenticationManager authenticationManager, UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @GetMapping("/findAll")
     public ResponseEntity<String> getAllUsers() throws JsonProcessingException {
@@ -56,14 +75,26 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("USUÁRIOS NÃO ENCONTRADOS");
     }
 
-    @PostMapping("/insert")
-    public ResponseEntity<String> createUser(@RequestBody User user) {
-        try {
-            userRepository.save(user);
-            return ResponseEntity.status(HttpStatus.CREATED).body("USUÁRIO CADASTRADO COM SUCESSO");
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("ERRO AO CADASTRAR USUÁRIO: " + e.getMessage());
+    @PostMapping("/register")
+    public ResponseEntity<String>createUser(@RequestBody RegisterDto registerDto) {
+
+        if (userRepository.existsByUserName(registerDto.getUsername())){
+            return new ResponseEntity<>("Username já existe!", HttpStatus.BAD_REQUEST);
         }
+
+        User user = new User();
+        user.setUserName(registerDto.getUsername());
+        user.setName(registerDto.getName());
+        user.setEmail(registerDto.getEmail());
+        user.setPassword(registerDto.getPassword());
+
+        Role roles = roleRepository.findByName("USER").get();
+
+        user.setRoles(Collections.singletonList(roles));
+
+        userRepository.save(user);
+
+        return new ResponseEntity<>("User registrado com sucesso", HttpStatus.OK);
     }
 
     @DeleteMapping("/deleteById/{id}")
@@ -85,11 +116,14 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("USUÁRIO NÃO ENCONTRADO");
     }
 
-    @GetMapping("/authenticate")
-//    @PostMapping("/authenticate"
-    public ResponseEntity<String> authenticateUser(@RequestParam String email, @RequestParam String password) {
-        Optional<User> user = userRepository.findByEmailAndPassword(email, password);
-        return user.map(u -> ResponseEntity.ok("AUTENTICAÇÃO BEM-SUCEDIDA"))
-                .orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("CREDENCIAIS INVÁLIDAS"));
+//    @GetMapping("/authenticate")
+    @PostMapping("/login")
+    public ResponseEntity<String> authenticateUser(@RequestParam String username, @RequestParam String password) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(username, password));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return new ResponseEntity<>("User logado com sucesso", HttpStatus.OK);
     }
 }
--- src/main/java/edu/meialua/kidsgrace/security/SecurityConfig.java
+++ src/main/java/edu/meialua/kidsgrace/security/SecurityConfig.java
@@ -1,13 +1,18 @@
 package edu.meialua.kidsgrace.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -17,36 +22,61 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    public SecurityConfig(CustomUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.GET, "/toys/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/users/**").hasRole("ADMIN")
-                        .anyRequest().authenticated()
+                        .requestMatchers(HttpMethod.GET, "/users/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/users/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/users/login").permitAll() // 🔥 Adicionado explicitamente
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .anyRequest().permitAll() // Permite tudo
                 )
 
-                .httpBasic(withDefaults());
+//                .httpBasic(withDefaults());
+                .formLogin(form -> form.disable()) // Desativa login por formulário
+                .httpBasic(basic -> basic.disable()); // Desativa Basic Auth
 
         return http.build();
     }
 
+
+
+//    @Bean
+//    public UserDetailsService users(){
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password("{noop}password")
+//                .roles("ADMIN")
+//                .build();
+//
+//        UserDetails user = User.builder()
+//                .username("user")
+//                .password("{noop}password")
+//                .roles("USER")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(admin, user);
+//    }
+
     @Bean
-    public UserDetailsService users(){
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password("{noop}password")
-                .roles("ADMIN")
-                .build();
+    public AuthenticationManager authenticationManager(
+        AuthenticationConfiguration authenticationConfiguration) throws Exception{
 
-        UserDetails user = User.builder()
-                .username("user")
-                .password("{noop}password")
-                .roles("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin, user);
+            return authenticationConfiguration.getAuthenticationManager();
+        }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
